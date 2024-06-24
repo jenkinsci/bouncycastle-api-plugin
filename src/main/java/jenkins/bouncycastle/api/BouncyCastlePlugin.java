58cf36d5a34d919e371c3f9f78ab2a0065019935
James Nord <jtnord@users.noreply.github.com>
2024-06-24 10:41:43 +0100

    enable spotless

--- src/main/java/jenkins/bouncycastle/api/BouncyCastlePlugin.java
+++ src/main/java/jenkins/bouncycastle/api/BouncyCastlePlugin.java
@@ -1,18 +1,18 @@
 package jenkins.bouncycastle.api;
 
-import java.io.File;
-import java.security.Provider;
-import java.security.Security;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.NoExternalUse;
 import hudson.Main;
 import hudson.Plugin;
 import hudson.PluginWrapper;
 import hudson.remoting.Which;
+import java.io.File;
 import java.io.IOException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jenkins.model.Jenkins;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 @SuppressWarnings("deprecation") // there is no other way to achieve this at the correct lifecycle point
 @Restricted(NoExternalUse.class) // just for Jenkins access not part of the API
@@ -27,9 +27,11 @@ public class BouncyCastlePlugin extends Plugin {
         Provider p = Security.getProvider("BCFIPS");
         isActive = (p == null);
 
-        LOG.log(Level.CONFIG,
-                isActive ? "BouncyCastle Providers from BouncyCastle API plugin will be active" :
-                           "Detected the presence of the BouncyCastle FIPS provider, the regular BouncyCastle JARs will not be available.");
+        LOG.log(
+                Level.CONFIG,
+                isActive
+                        ? "BouncyCastle Providers from BouncyCastle API plugin will be active"
+                        : "Detected the presence of the BouncyCastle FIPS provider, the regular BouncyCastle JARs will not be available.");
     }
 
     @Override
@@ -50,9 +52,15 @@ public class BouncyCastlePlugin extends Plugin {
 
         if (optionalLibs == null || optionalLibs.length == 0) {
             if (Main.isUnitTest) {
-                LOG.log(Level.INFO, "{0} not found; for non RealJenkinsRule this is fine and can be ignored.", optionalLibDir);
+                LOG.log(
+                        Level.INFO,
+                        "{0} not found; for non RealJenkinsRule this is fine and can be ignored.",
+                        optionalLibDir);
             } else {
-                LOG.log(Level.WARNING, "No optional-libs not found at {0}, BouncyCastle APIs will be unavailable causing strange runtime issues.", optionalLibDir);
+                LOG.log(
+                        Level.WARNING,
+                        "No optional-libs not found at {0}, BouncyCastle APIs will be unavailable causing strange runtime issues.",
+                        optionalLibDir);
                 // fail fast, most likely a packaging issue
                 throw new IllegalStateException("BouncyCastle libs are missing from WEB-INF/optional-libs");
             }
@@ -66,16 +74,18 @@ public class BouncyCastlePlugin extends Plugin {
         return isActive;
     }
 
-
     private final File getOptionalLibDirectory() throws IOException {
         PluginWrapper pw = getWrapper();
         File explodedPluginsDir = pw.parent.getWorkDir();
         if (explodedPluginsDir == null) {
             // not overridden use default of ${JENKINS_HOME}/plugins
             explodedPluginsDir = new File(Jenkins.get().getRootDir(), "plugins");
-            LOG.log(Level.FINE, "plugindir not specified, falling back to $'{'JENKINS_HOME/plugins'}' as {0}", explodedPluginsDir);
+            LOG.log(
+                    Level.FINE,
+                    "plugindir not specified, falling back to $'{'JENKINS_HOME/plugins'}' as {0}",
+                    explodedPluginsDir);
         }
-        File f =  new File(explodedPluginsDir, pw.getShortName() + "/WEB-INF/optional-lib/");
+        File f = new File(explodedPluginsDir, pw.getShortName() + "/WEB-INF/optional-lib/");
         if (!f.isDirectory()) {
             File here = Which.jarFile(BouncyCastlePlugin.class);
             File alt = new File(here.getParentFile().getParentFile(), "optional-lib");
